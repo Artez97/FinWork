7e200073cb54bbdfa66ac1155cf97437b2525923
Artem <ermilintemka@gmail.com>
2022-08-08 18:43:58 +0300

    add programm

--- Exp/Program.cs
+++ Exp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array;
+Console.WriteLine("Введите строки через пробел");
+string row = Console.ReadLine();
+array = row.Split(' ');
+var res = new string[array.Length];
+var size = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        res[size] = value;
+        size++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, res, 0, size));
+Console.ReadKey(true);
